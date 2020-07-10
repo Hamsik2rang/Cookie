d10e49efa16195504487f46dea50eda4a669f79b
Im Yongsik <lvhi7121@gmail.com>
2020-07-10 23:19:29 +0900

    [#2] Animation FSM 정리

---
+++ GameClient/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public bool isGameOver = false;
+    public Text turnText; 
+    private int turnCount = 0;
+
+
+
+    private void Awake()
+    {   
+        //싱글턴 구현
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
